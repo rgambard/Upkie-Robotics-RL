8c4ca7925edfd99d82bf54baf1f705eb9797a5d5
John Doe <johndoe@example.com>
2024-11-19 14:47:27 +0100

    added subject

--- test.py
+++ test.py
@@ -20,9 +20,9 @@ with gym.make("UpkieGroundVelocity-v3", frequency=200.0) as env:
         action = gain.dot(observation).reshape((1,))
         observation, reward, terminated, truncated, _ = env.step(action)
         if step>=100 and step<=300:
-            torso_force_in_world[0] = 10
+            torso_force_in_world[0] = 7
         else:
             torso_force_in_world[0] = 0
-        env.bullet_extra(bullet_action)
+        env.env.env.bullet_extra(bullet_action)
         if terminated or truncated:
             observation,_ = env.reset()
