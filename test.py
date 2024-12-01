6ef50280f7798de7150de11681e60b66f02f869b
John Doe <johndoe@example.com>
2024-12-01 14:49:05 +0100

    added policies description

--- test.py
+++ test.py
@@ -15,14 +15,15 @@ with gym.make("UpkieGroundVelocity-v3", frequency=200.0) as env:
         }
     }
     observation, _ = env.reset()
-    gain = np.array([20.0, 0.0, 0.0, 0.0])
+    gain = np.array([30.0, 0, 1.0, 1.0])
     for step in range(1_000_000):
         action = gain.dot(observation).reshape((1,))
         observation, reward, terminated, truncated, _ = env.step(action)
         if step>=100 and step<=300:
-            torso_force_in_world[0] = 7
+            torso_force_in_world[0] = -8
         else:
             torso_force_in_world[0] = 0
-        env.env.env.bullet_extra(bullet_action)
+        env.bullet_extra(bullet_action)
         if terminated or truncated:
-            observation,_ = env.reset()
+            break
+            observation,_ = env.reset() 
